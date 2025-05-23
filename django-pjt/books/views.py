28729cdff840dd417748b4822d398d900be15884
Slo1024 <jehwju1024@naver.com>
2025-05-23 11:09:16 +0900

    도서 전체 조회/상세 조회

--- django-pjt/books/views.py
+++ django-pjt/books/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# 책 목록 조회 API
+class BookListView(APIView):
+    def get(self, request):
+        # 모든 책을 가져옴
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+# 책 상세 정보 조회 API
+class BookDetailAPIView(APIView):
+    def get(self, request, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+            serializer = BookSerializer(book)
+            return Response(serializer.data)
+        except Book.DoesNotExist:
+            return Response({"detail": "Book not found"}, status=404)
