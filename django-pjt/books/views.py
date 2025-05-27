ba389d55f131b627f0fac62314ea8998d33bdaba
Slo1024 <jehwju1024@naver.com>
2025-05-27 17:56:10 +0900

    wikiapi_author info

--- django-pjt/books/views.py
+++ django-pjt/books/views.py
@@ -6,9 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer, BookDetailSerializer
-from .utils.wiki_api import get_wikipedia_content, get_wikipedia_image
-from django.core.files.base import ContentFile
-# from threads.serializers import ThreadSerializer
+from .utils.wiki_api import process_author_info_by_book_pk
 
 # 책 목록 조회
 # URL: api/v1/books/
@@ -30,25 +28,21 @@ def book_detail(request, pk):
 # URL: api/v1/books/<int:pk>/author/
 @api_view(['GET'])
 def author_info_by_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    author_name = book.author
+    """
+    book pk를 받아서 위키피디아+GPT 기반으로 작가 정보(소개, 대표작, 사진 등) 반환
+    """
+    # DB에서 Book 존재 확인 및 작가 정보 처리 함수 호출
+    author_data = process_author_info_by_book_pk(pk)
+    if author_data is None:
+        return Response({"detail": "책 또는 작가 정보를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+    
+    # author_data 예시 키:
+    # {
+    #   "author_name": "홍길동",
+    #   "author_info": "홍길동은 ...",
+    #   "author_works": "대표작1, 대표작2, 대표작3",
+    #   "author_photo_url": "/media/author_profiles/author_10_홍길동.jpg",
+    #   "wiki_url": "https://ko.wikipedia.org/wiki/홍길동_(작가)"
+    # }
 
-    author_obj, created = Author.objects.get_or_create(name=author_name)
-
-    if created or not author_obj.bio or not author_obj.photo:
-        # Wikipedia에서 작가 정보 가져오기
-        wiki_data = get_wikipedia_content(author_name)
-        if wiki_data:
-            author_obj.bio = wiki_data.get("summary", "")
-
-        img_url = get_wikipedia_image(author_name)
-        if img_url:
-            response_img = requests.get(img_url)
-            if response_img.status_code == 200:
-                filename = os.path.basename(img_url)
-                author_obj.photo.save(filename, ContentFile(response_img.content))  # save() 호출
-                
-        author_obj.save()
-
-    serializer = AuthorSerializer(author_obj, context={'request': request})
-    return Response(serializer.data)
+    return Response(author_data)
